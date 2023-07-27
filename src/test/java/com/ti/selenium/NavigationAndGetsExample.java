da22380eddcee6ebf167385963c40f1d30361393
gsanchezm <gilsanchezts@outlook.com>
2023-07-26 21:29:41 -0600

    added examples from session 26/07/2023 Navigation and get commands

---
+++ src/test/java/com/ti/selenium/NavigationAndGetsExample.java
@@ -0,0 +1,60 @@
+package com.ti.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NavigationAndGetsExample {
+    WebDriver driver;
+    String demoSiteUrl = "https://demosite.titaniuminstitute.com.mx/wp-admin/admin.php?page=sch-dashboard";
+    String googleSite = "https://www.google.com";
+
+    @BeforeTest
+    void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    void turnDown() {
+        driver.quit();
+    }
+
+    @Test
+    void validateNavigationAndGetCommands() {
+        driver.navigate().to(googleSite);
+
+        System.out.println(String.format("The Web page title is %s", driver.getTitle()));
+        System.out.println(String.format("The URL is %s", driver.getCurrentUrl()));
+
+        wait(2);
+
+        driver.get(demoSiteUrl);
+
+        System.out.println("***********************************************");
+        System.out.println(String.format("The Web page title is %s", driver.getTitle()));
+        System.out.println(String.format("The URL is %s", driver.getCurrentUrl()));
+        System.out.println(String.format("The source code is %s", driver.getPageSource()));
+
+        wait(2);
+
+        driver.navigate().back();
+
+        wait(2);
+
+        driver.navigate().forward();
+
+        wait(2);
+    }
+
+    void wait(int seg) {
+        try {
+            Thread.sleep(seg * 1000);
+        } catch (InterruptedException ie) {
+            System.err.println(ie.getMessage());
+        }
+    }
+}
---
+++ src/test/java/com/ti/selenium/SeleniumExceptions.java
@@ -0,0 +1,49 @@
+package com.ti.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumExceptions {
+
+    // https://www.selenium.dev/selenium/docs/api/py/common/selenium.common.exceptions.html
+    WebDriver driver;
+    String demoSiteUrl = "www.demosite.titaniuminstitute.com.mx/wp-admin/admin.php?page=sch-dashboard";
+    String username = "admin";
+
+    @BeforeTest
+    void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void firstTest() throws InterruptedException {
+        try {
+            driver.navigate().to(demoSiteUrl);
+        } catch (InvalidArgumentException iae) {
+            driver.navigate().to("https://" + demoSiteUrl);
+        }
+
+        try {
+            driver.findElement(By.id("user_logi")).sendKeys(username);
+            Assert.assertTrue(true);
+        } catch (NoSuchElementException nse) {
+            Assert.fail("Test Failed: element is not found: " + nse.getMessage());
+        }
+
+
+    }
+
+    @AfterTest
+    void turnDown() {
+        driver.quit();
+    }
+}
