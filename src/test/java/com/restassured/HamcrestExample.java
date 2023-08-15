426b1951ee062b57e14a15a374bb363030a699be
gsanchezm <gilsanchezts@outlook.com>
2023-08-14 20:41:49 -0600

    added examples from session 14/08/2023 Matchers Numbers

--- src/test/java/com/restassured/HamcrestExample.java
+++ src/test/java/com/restassured/HamcrestExample.java
@@ -3,6 +3,8 @@ package com.restassured;
 import com.google.common.collect.Lists;
 import org.testng.annotations.Test;
 
+import java.math.BigDecimal;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -71,4 +73,40 @@ public class HamcrestExample {
         assertThat(favoriteCar,
                 stringContainsInOrder(Lists.newArrayList("Masserati", "Lamborgini", "Mercedez", "BMW")));
     }
+
+    // Number Matchers
+    @Test
+    void greaterThanInteger() {
+        assertThat(2, greaterThan(1));
+    }
+
+    @Test
+    void greaterThanOrEqualInteger() {
+        assertThat(10, greaterThanOrEqualTo(9));
+    }
+
+    @Test
+    void lessThanInteger() {
+        assertThat(-1, lessThan(1));
+    }
+
+    @Test
+    void lessThanOrEqualsTo() {
+        assertThat(-1, lessThanOrEqualTo(-1));
+    }
+
+    @Test
+    void closeToDoubleNumber() {
+        // 1.0 (+/-) 0.8 to 1.2
+        assertThat(0.8, closeTo(1.0, 0.2));
+    }
+
+    @Test
+    void givenBigDecimalCloseTo() {
+        //2.0 (+/-) 1.5 to 2.5
+        assertThat(new BigDecimal("1.8"), is(closeTo(new BigDecimal("2.0"), new BigDecimal("0.5"))));
+    }
+
+    // Collections Matchers
+    
 }
