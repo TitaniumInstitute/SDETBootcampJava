b4da7f6c33fee547c7682181f56ba56c88f58fd0
Dulce Karen Gonzalez <dgonzalez@applab.mx>
2023-09-04 21:43:50 -0600

    Session 04/09/2023 DriverFactory final

--- src/test/java/com/ti/appium/BaseTest.java
+++ src/test/java/com/ti/appium/BaseTest.java
@@ -6,11 +6,26 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     @BeforeTest
-    public void setup(){
-        MobileDriverFactory.setAppName("Android.SauceLabs.Mobile.Sample.apk");
+    @Parameters({"deviceOsType","appName","ipAddress","deviceName"})
+    public void setup(String deviceOsType, String appName, String ipAddress, String deviceName){
+    //public void setup(String ... deviceSetup){
+        MobileDriverFactory.setAppName(appName);
+        MobileDriverFactory.setDeviceName(deviceName);
+        AppiumDriverLocalService service = new AppiumServiceBuilder()
+                .withArgument(() -> "--use-plugins", "element-wait@2.0.3,gestures@3.0.0")
+                .withIPAddress(ipAddress)
+                .usingAnyFreePort()
+                .build();
+
+        if (deviceOsType.equals(DeviceOSType.ANDROID.toString())){
+            MobileDriverFactory.setAppActivity("com.swaglabsmobileapp.MainActivity");
+        }
+        MobileDriverFactory.getInstance().setMobileDriver(DeviceOSType.valueOf(deviceOsType),service);
+        /*MobileDriverFactory.setAppName("Android.SauceLabs.Mobile.Sample.apk");
         MobileDriverFactory.setDeviceName("S21 plus API 30");
         AppiumDriverLocalService service = new AppiumServiceBuilder()
                 .withIPAddress("127.0.0.1")
@@ -18,7 +33,7 @@ public class BaseTest {
                 .build();
 
         MobileDriverFactory.setAppActivity("com.swaglabsmobileapp.MainActivity");
-        MobileDriverFactory.getInstance().setMobileDriver(DeviceOSType.ANDROID,service);
+        MobileDriverFactory.getInstance().setMobileDriver(DeviceOSType.ANDROID,service);*/
     }
 
     @AfterTest
